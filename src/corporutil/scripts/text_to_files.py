8bf9fa4677c19af63f92f0e105f666c0c1d6f428
dcronkite <dcronkite@gmail.com>
2023-06-27 11:05:26 -0700

    Option to ensure that all generated files end in a newline

--- src/corporutil/scripts/text_to_files.py
+++ src/corporutil/scripts/text_to_files.py
@@ -22,17 +22,24 @@ from corporutil.dataiter import get_documents_from_source
               help='Extension of text files to be created.')
 @click.option('--text-encoding', default='utf8',
               help='Encoding for writing text files.')
+@click.option('--ensure-newline', is_flag=True, default=False,
+              help='Ensure that each text file has a newline;'
+                   ' certain programs (e.g., Metamap) will fail without newlines.')
 @click.option('--force', is_flag=True, default=False)
-def text_from_file(outdir: Path, n_dirs=1, text_extension='.txt', text_encoding='utf8', force=False, **fileargs):
+def text_from_file(outdir: Path, n_dirs=1, text_extension='.txt', text_encoding='utf8', force=False,
+                   ensure_newline=False, **fileargs):
     it = get_documents_from_source(**fileargs)
     build_files(it, outdir=outdir, n_dirs=n_dirs, text_extension=text_extension, text_encoding=text_encoding,
                 force=force)
 
 
-def build_files(text_gen, outdir: Path, n_dirs=1, text_extension='.txt', text_encoding='utf8', force=False):
+def build_files(text_gen, outdir: Path, n_dirs=1, text_extension='.txt', text_encoding='utf8',
+                ensure_newline=False, force=False):
     """
     Write files to directory from generator outputting (note_id, text).
         A filelist will also be created for each outdirectory.
+    :param ensure_newline: ensure that file ends with newline before writing
+    :param force: force overwriting existing directory
     :param text_gen:
     :param outdir:
     :param n_dirs:
@@ -51,6 +58,8 @@ def build_files(text_gen, outdir: Path, n_dirs=1, text_extension='.txt', text_en
                  for i in range(n_dirs)]
     for i, (note_id, text) in enumerate(text_gen):
         outfile = outdirs[i % n_dirs] / f'{note_id}{text_extension}'
+        if ensure_newline and not text.endswith('\n'):  # ensure that every text files ends in a newline
+            text += '\n'
         with open(outfile, 'w', encoding=text_encoding, errors='replace') as out:
             out.write(text)
         filelists[i % n_dirs].write(f'{outfile.absolute()}\n')
