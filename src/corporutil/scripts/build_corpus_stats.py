2d42fd5320f135c05b5e6223a7a442d1c0a1a800
dcronkite <dcronkite@gmail.com>
2023-06-28 15:06:06 -0700

    Replaced `open` with `FileWriter` function

--- src/corporutil/scripts/build_corpus_stats.py
+++ src/corporutil/scripts/build_corpus_stats.py
@@ -4,15 +4,16 @@ from pathlib import Path
 
 import click
 
-from corporutil.args import retrieve_text_options, outfile_arg
+from corporutil.args import retrieve_text_options, outdir_arg
 from corporutil.dataiter import get_documents_from_source
+from corporutil.fileio import FileWriter
 from corporutil.nlp.spacy import get_spacy_or_none
 from corporutil.stats import build_stats, build
 
 
 @click.command()
 @retrieve_text_options
-@outfile_arg
+@outdir_arg
 @click.option('--spacy-model', default=None,
               help='Specify spacy model')
 @click.option('--patient-col', type=int, default=None,
@@ -23,26 +24,26 @@ from corporutil.stats import build_stats, build
               help='Specify patient column index.')
 @click.option('--kind', default='json',
               help='Specify json or csv output types.')
-def build_corpus_stats(outfile: Path, patient_col, encounter_col, text_col=-1, spacy_model=None, kind='json',
+def build_corpus_stats(outdir: Path, patient_col, encounter_col, text_col=-1, spacy_model=None, kind='json',
                        **fileargs):
     it = get_documents_from_source(**fileargs)
     nlp = get_spacy_or_none(spacy_model)
     if kind == 'json':
-        build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outfile)
+        build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outdir)
     elif kind == 'csv':
-        build_corpus_stats_csv(it, nlp, outfile)
+        build_corpus_stats_csv(it, nlp, outdir)
     else:
         raise ValueError(f'Unrecognized kind: {kind}')
 
 
-def build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outfile):
+def build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outdir):
     results = build_stats(it, text_col=text_col, enc_col=encounter_col, pt_col=patient_col, nlp=nlp)
-    with open(outfile, 'w', encoding='utf8') as fh:
+    with FileWriter(outdir, 'summary', ext='.json') as fh:
         json.dump(results, fh, indent=2)
 
 
-def build_corpus_stats_csv(it, nlp, outfile):
-    with open(outfile, 'w', encoding='utf8', newline='') as fh:
+def build_corpus_stats_csv(it, nlp, outdir):
+    with FileWriter(outdir, 'summary', ext='.csv', newline='') as fh:
         writer = csv.writer(fh)
         for i, (*meta, text) in enumerate(it):
             docstats = build(text, nlp=nlp)
