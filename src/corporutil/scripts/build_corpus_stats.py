305d45a59fbef62188caf919bd1a1c9d2840bec3
dcronkite <dcronkite@gmail.com>
2023-06-28 15:03:57 -0700

    Updated corpus stats to provide a csv output option as well

--- src/corporutil/scripts/build_corpus_stats.py
+++ src/corporutil/scripts/build_corpus_stats.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from pathlib import Path
 
@@ -6,7 +7,7 @@ import click
 from corporutil.args import retrieve_text_options, outfile_arg
 from corporutil.dataiter import get_documents_from_source
 from corporutil.nlp.spacy import get_spacy_or_none
-from corporutil.stats import build_stats
+from corporutil.stats import build_stats, build
 
 
 @click.command()
@@ -20,12 +21,34 @@ from corporutil.stats import build_stats
               help='Specify patient column index.')
 @click.option('--text-col', type=int, default=-1,
               help='Specify patient column index.')
-def build_corpus_stats(outfile: Path, patient_col, encounter_col, text_col=-1, spacy_model=None, **fileargs):
+@click.option('--kind', default='json',
+              help='Specify json or csv output types.')
+def build_corpus_stats(outfile: Path, patient_col, encounter_col, text_col=-1, spacy_model=None, kind='json',
+                       **fileargs):
     it = get_documents_from_source(**fileargs)
     nlp = get_spacy_or_none(spacy_model)
+    if kind == 'json':
+        build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outfile)
+    elif kind == 'csv':
+        build_corpus_stats_csv(it, nlp, outfile)
+    else:
+        raise ValueError(f'Unrecognized kind: {kind}')
+
+
+def build_corpus_stats_json(it, text_col, encounter_col, patient_col, nlp, outfile):
     results = build_stats(it, text_col=text_col, enc_col=encounter_col, pt_col=patient_col, nlp=nlp)
     with open(outfile, 'w', encoding='utf8') as fh:
         json.dump(results, fh, indent=2)
+
+
+def build_corpus_stats_csv(it, nlp, outfile):
+    with open(outfile, 'w', encoding='utf8', newline='') as fh:
+        writer = csv.writer(fh)
+        for i, (*meta, text) in enumerate(it):
+            docstats = build(text, nlp=nlp)
+            if i == 0:
+                writer.writerow([f'meta{i}' for i in range(len(meta))] + list(docstats.__slots__))
+            writer.writerow(meta + [getattr(docstats, v) for v in docstats.__slots__])
 
 
 if __name__ == '__main__':
