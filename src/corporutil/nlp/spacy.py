421af6e0c187a81e703d724d70fda258ef3f366f
dcronkite <dcronkite@gmail.com>
2024-09-23 16:56:16 -0700

    Handle if spacy not installed

--- src/corporutil/nlp/spacy.py
+++ src/corporutil/nlp/spacy.py
@@ -1,7 +1,16 @@
-import spacy
+from loguru import logger
+
+SPACY = True
+try:
+    import spacy
+except ImportError:
+    SPACY = False
 
 
 def get_spacy_or_none(model=None):
+    if not SPACY:
+        logger.warning(f'Spacy not found: unable to use spacy models.')
+        return None
     if model is not None:
         return spacy.load(model)  # fail if specified model not found
     for model in ['en_core_web_sm', 'en_core_web_md', 'en_core_web_lg', 'en_core_web_trf']:
